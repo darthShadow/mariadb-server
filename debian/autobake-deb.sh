4a29a1352e3c2d804636363fc857c6802cfe9200
Anagh Kumar Baranwal <6824881+darthShadow@users.noreply.github.com>
2022-05-05 11:54:36 +0530

    Revert "MDEV-28011: debian autobake cleanup"
    This reverts commit 6de482a6fefac0c21daf33ed465644151cdf879f.
    
    Conflicts:
        debian/autobake-deb.sh
        debian/control

--- debian/autobake-deb.sh
+++ debian/autobake-deb.sh
@@ -11,6 +11,16 @@ set -e
 # Buildbot, running the test suite from installed .debs on a clean VM.
 export DEB_BUILD_OPTIONS="nocheck"
 
+# Don't include test suite package to make the build time shorter
+sed '/Package: mariadb-test-data/,/^$/d' -i debian/control
+sed '/Package: mariadb-test/,/^$/d' -i debian/control
+
+export MYSQL_COMPILER_LAUNCHER=ccache
+
+# Don't include TokuDB since it's deprecated
+sed '/Package: mariadb-plugin-tokudb/,/^$/d' -i debian/control
+sed 's|-DDEB|-DPLUGIN_TOKUDB=NO -DDEB|' -i debian/rules
+
 # Look up distro-version specific stuff
 #
 # Always keep the actual packaging as up-to-date as possible following the latest
@@ -25,12 +35,50 @@ if [ -z "${LSBID}" ]
 then
     LSBID="unknown"
 fi
+
 case "${LSBNAME}" in
-	stretch)
-		# MDEV-28022 libzstd-dev-1.1.3 minimum version
-		sed -i -e '/libzstd-dev/d' debian/control
-		;;
+  stretch)
+    # MDEV-28022 libzstd-dev-1.1.3 minimum version
+    sed -i -e '/libzstd-dev/d' debian/control
+    ;;
 esac
+
+# If iproute2 is not available (before Debian Jessie and Ubuntu Trusty)
+# fall back to the old iproute package.
+if ! apt-cache madison iproute2 | grep 'iproute2 *|' >/dev/null 2>&1
+then
+  sed 's/iproute2/iproute/' -i debian/control
+fi
+
+# If libcrack2 (>= 2.9.0) is not available (before Debian Jessie and Ubuntu Trusty)
+# clean away the cracklib stanzas so the package can build without them.
+if ! apt-cache madison libcrack2-dev | grep 'libcrack2-dev *| *2\.9' >/dev/null 2>&1
+then
+  sed '/libcrack2-dev/d' -i debian/control
+  sed '/Package: mariadb-plugin-cracklib/,/^$/d' -i debian/control
+fi
+
+# If libpcre3-dev (>= 2:8.35-3.2~) is not available (before Debian Jessie or Ubuntu Wily)
+# clean away the PCRE3 stanzas so the package can build without them.
+# Update check when version 2:8.40 or newer is available.
+if ! apt-cache madison libpcre3-dev | grep 'libpcre3-dev *| *2:8\.3[2-9]' >/dev/null 2>&1
+then
+  sed '/libpcre3-dev/d' -i debian/control
+fi
+
+# If libsystemd-dev is not available (before Debian Jessie or Ubuntu Wily)
+# clean away the systemd stanzas so the package can build without them.
+if ! apt-cache madison libsystemd-dev | grep 'libsystemd-dev' >/dev/null 2>&1
+then
+  sed '/dh-systemd/d' -i debian/control
+  sed '/libsystemd-dev/d' -i debian/control
+  sed 's/ --with systemd//' -i debian/rules
+  sed '/systemd/d' -i debian/rules
+  sed '/\.service/d' -i debian/rules
+  sed '/galera_new_cluster/d' -i debian/mariadb-server-10.4.install
+  sed '/galera_recovery/d' -i debian/mariadb-server-10.4.install
+  sed '/mariadb-service-convert/d' -i debian/mariadb-server-10.4.install
+fi
 
 # Don't build rocksdb package on x86 32 bit.
 if [[ $(arch) =~ i[346]86 ]]
@@ -45,12 +93,21 @@ then
 fi
 
 # Always remove aws plugin, see -DNOT_FOR_DISTRIBUTION in CMakeLists.txt
-sed '/Package: mariadb-plugin-aws-key-management-10.2/,/^$/d' -i debian/control
+sed '/Package: mariadb-plugin-aws-key-management-10.4/,/^$/d' -i debian/control
 
 # Don't build cassandra package if thrift is not installed
 if [[ ! -f /usr/local/include/thrift/Thrift.h && ! -f /usr/include/thrift/Thrift.h ]]
 then
   sed '/Package: mariadb-plugin-cassandra/,/^$/d' -i debian/control
+fi
+
+# From Debian Stretch/Ubuntu Bionic onwards dh-systemd is just an empty
+# transitional metapackage and the functionality was merged into debhelper.
+# In Ubuntu Hirsute is was completely removed, so it can't be referenced anymore.
+# Keep using it only on Debian Jessie and Ubuntu Xenial.
+if apt-cache madison dh-systemd | grep 'dh-systemd' >/dev/null 2>&1
+then
+  sed 's/debhelper (>= 9.20160709~),/debhelper (>= 9), dh-systemd,/' -i debian/control
 fi
 
 # Adjust changelog, add new version
@@ -67,6 +124,10 @@ VERSION="${EPOCH}${UPSTREAM}${PATCHLEVEL}~${LSBID:0:3}${LSBVERSION}"
 dch -b -D ${LSBNAME} -v "${VERSION}" "Automatic build with ${LOGSTRING}."
 
 echo "Creating package version ${VERSION} ... "
+
+# Use -b to build binary only packages as there is no need to
+# waste time on generating the source package.
+BUILDPACKAGE_FLAGS="-b"
 
 # Build the package
 # Pass -I so that .git and other unnecessary temporary and source control files
